cc76da02875b4cefdd40c1802a607e2595336962
Denis Paskin <paskin.denis.00@gmail.com>
2022-05-31 10:16:38 +0200

    modified the script

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = 'my project', 
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 100,
+    rollback = 44,
+    fullPrice = 2000, 
+    adaptive = true;
 
 
-alert('Hi');
-console.log('hello');
+    console.log(typeof title);
+    console.log(typeof fullPrice);
+    console.log(typeof adaptive);
+    console.log(screens.length);
+    console.log(`Стоимость вёрстки экранов ${screenPrice}$`);
+    console.log(`Стоимость разработки сайтов ${fullPrice}$`);
+    console.log(screens.toLowerCase().split());
+    console.log(fullPrice * (rollback / 100));
+
+// alert('Hi');
+// console.log('hello');
