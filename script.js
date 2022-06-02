1b5b84811a133461262290167f971907dd065093
Denis Paskin <paskin.denis.00@gmail.com>
2022-06-02 09:59:06 +0200

    modified script

--- script.js
+++ script.js
@@ -13,9 +13,16 @@ let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let rollback = 24;
 let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
 
-console.log(servicePercentPrice);
-
 if (fullPrice >= 30000) console.log('Даём скидку в 10%');
 if (fullPrice >= 15000) console.log('Даём скидку в 5%');
 if (fullPrice < 15000 && fullPrice > 0) console.log('Скидка не предусмотрена');
 if (fullPrice <= 0) console.log('Что то пошло не так');
+
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(servicePercentPrice);
+
+console.log(`Стоимость вёрстки экранов ${screenPrice} рублей/ долларова/ гривен/ юаней`);
