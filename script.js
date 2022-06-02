711d1305842a3e79ef3285902f1c300afd3a414f
Denis Paskin <paskin.denis.00@gmail.com>
2022-06-02 23:34:18 +0200

    completed lesson 4

--- script.js
+++ script.js
@@ -11,17 +11,47 @@ let service2 = prompt('Какой дополнительный тип услуг
 let servicePrice2 = +prompt('Сколько это будет стоить?');
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let rollback = 24;
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+let allServicePrices;
 
-if (fullPrice >= 30000) console.log('Даём скидку в 10%');
-if (fullPrice >= 15000) console.log('Даём скидку в 5%');
-if (fullPrice < 15000 && fullPrice > 0) console.log('Скидка не предусмотрена');
-if (fullPrice <= 0) console.log('Что то пошло не так');
 
-console.log(typeof title);
-console.log(typeof screenPrice);
-console.log(typeof adaptive);
+const getAllServicePrices = function () {
+    allServicePrices = servicePrice1 + servicePrice2;
+    return allServicePrices;
+};
 
-console.log(screens.length);
-console.log(servicePercentPrice);
-console.log(`Стоимость вёрстки экранов ${screenPrice} рублей/ долларова/ гривен/ юаней и Стоимость разработки сайтов ${fullPrice} рублей/ долларова/ гривен/ юаней`);
+function getFullPrice() {
+    fullPrice = screenPrice + allServicePrices;
+    return fullPrice;
+}
+
+const getTitle = function() {
+   return (title[0] != ' ') ? title[0].toUpperCase() + title.slice(1) :
+                       console.log(title[1].toUpperCase() + title.slice(2));
+};
+
+const getServicePercentPrices = function() {
+    servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+    return servicePercentPrice;
+};
+
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+};
+
+const getRollbackMessage = function (price) {
+    if (price >= 30000) return 'Даём скидку в 10%';
+    if (price >= 15000) return 'Даём скидку в 5%';
+    if (price < 15000 && price > 0) return 'Скидка не предусмотрена';
+    if (price <= 0) return 'Что то пошло не так';
+};
+
+getServicePercentPrices();
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
+
+console.log(screens);
+
