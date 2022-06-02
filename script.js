b2cbea8a5eb8af3d377bdf6bd5d41ae22ca951cf
Denis Paskin <paskin.denis.00@gmail.com>
2022-06-02 09:50:56 +0200

    modified script

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
+'use strict';
 
-//lesson02
-let title = 'my project', 
-    screens = "Простые, Сложные, Интерактивные",
-    screenPrice = 100,
-    rollback = 44,
-    fullPrice = 2000, 
-    adaptive = true;
-
-
-    // console.log(typeof title);
-    // console.log(typeof fullPrice);
-    // console.log(typeof adaptive);
-    // console.log(screens.length);
-    // console.log(`Стоимость вёрстки экранов ${screenPrice}$`);
-    // console.log(`Стоимость разработки сайтов ${fullPrice}$`);
-    // console.log(screens.toLowerCase().split(', '));
-    // console.log(fullPrice * (rollback / 100));
-
-
-
- //lesson03
-    title = prompt('Как называется ваш проект?');
-    screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-    screenPrice = +prompt('Сколько будет стоить данная работа ?', '1200');
-    adaptive = confirm('Нужен ли адаптив на сайте?');
+//lesson03
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt('Сколько будет стоить данная работа ?', '1200');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
-    fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let rollback = 24;
 let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
 
 console.log(servicePercentPrice);
