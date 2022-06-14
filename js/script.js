149921da66836c9c20ad2ffb81dcec97419a402e
Denis Paskin <paskin.denis.00@gmail.com>
2022-06-14 11:18:11 +0200

    completed lesson 9

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const title = document.getElementsByTagName('title');
+const btn = document.getElementsByClassName('handler_btn');
+const plus = document.querySelector('.screen-btn');
+const percent = document.querySelectorAll('.percent');
+const number = document.querySelectorAll('.number');
+const range = document.querySelector('.rollback > div > [type="range"]');
+const span = document.querySelector('.rollback > div > .range-value');
+const total1 = document.getElementsByClassName('total-input')[0];
+const total2 = document.getElementsByClassName('total-input')[1];
+const total3 = document.getElementsByClassName('total-input')[2];
+const total4 = document.getElementsByClassName('total-input')[3];
+const total5 = document.getElementsByClassName('total-input')[4];
+
+let screen = document.querySelectorAll('.screen');
+
